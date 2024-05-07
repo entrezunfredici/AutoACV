bfb29d0a8f2bda0bc2ec5a8f47ad442f022dab37
entrezunfredici <superfred2468@gmail.com>
2024-05-07 10:54:05 +0200

    [ADD] databaseInformation ⚠ hostname is not added

--- api-auto-acv/db.config.js
+++ api-auto-acv/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     hostname: "",
-    username: "",
-    password: "",
-    database: "",
+    username: "root",
+    password: "root",
+    database: "autoACVDatabase",
     port: 3306
 }
 //les donées si dessus seront a adapter en fonction de la configuration du serveur nginx
