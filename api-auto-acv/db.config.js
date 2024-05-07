31fddf2d7e8d97a7ba84f29c27fbafe8e0ffdb24
entrezunfredici <superfred2468@gmail.com>
2024-05-07 09:52:33 +0200

    [ADD] app configuration ⚠ database is not configured yet

---
+++ api-auto-acv/db.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    hostname: "",
+    username: "",
+    password: "",
+    database: "",
+    port: 3306
+}
+//les donées si dessus seront a adapter en fonction de la configuration du serveur nginx
---
+++ api-auto-acv/models/index.js
@@ -0,0 +1,14 @@
+const { Sequelize } = require('sequelize');
+const dbConfig = require('../db.config');
+
+const instance = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+    host: dbConfig.hostname,
+    dialect: 'mysql',
+    port: dbConfig.port
+});
+
+module.exports = {
+    instance,
+    /*to add an model type:
+    model: require('./model')(instance)*/
+}
