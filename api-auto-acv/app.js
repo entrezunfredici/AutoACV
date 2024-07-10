1d4a9f6d89886a00e570178679f2b4e687267ec4
entrezunfredici <superfred2468@gmail.com>
2024-07-10 11:31:22 +0200

    [ADD] cors and script

--- api-auto-acv/app.js
+++ api-auto-acv/app.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express')
 const OpenApiValidalidator = require('express-openapi-validator');
 const app = express()
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(express.json())
 
