001dfbb3737efecc24cf21899cd80cb5217a5792
entrezunfredici <superfred2468@gmail.com>
2024-07-15 00:42:09 +0200

    [IMP] little update

--- api-auto-acv/.next/static/G3FQa8w-uBONR8oVBaKY3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-d2188a0388e525fb.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
